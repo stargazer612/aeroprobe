34e632c2bbf1afa6e6206513b155b84b01680c56
stargazer612 <59992692+stargazer612@users.noreply.github.com>
2021-05-22 18:20:40 +0530

    Update calform.js

--- src/components/pages/calform.js
+++ src/components/pages/calform.js
@@ -8,7 +8,7 @@ const calform=(props)=>{
             
             <form onSubmit={props.getCity}>
              <div style ={{textAlign:'center'}}className="form-inner" >
-                <label className="formcitylabel" style={{fontSize:"40px"}} htmlFor="username">City</label>
+                <label className="formcitylabel" style={{fontSize:"40px"}} htmlFor="username">Location</label>
                 <input className="cityinput" style={{marginLeft:"20px",width:"250px",height:"50px"}} type="text" name="city"  defaultValue={localStorage.getItem("city")}/>
                 <button  className="Button3">Search</button>            
           </div>
